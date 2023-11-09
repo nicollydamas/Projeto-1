c620f18ef15bfd6d39578695ac91e119599f44d1
nicollydamas <149096690+nicollydamas@users.noreply.github.com>
2023-11-09 13:58:52 -0300

    Adicionando arquivos, html, js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(400, 400);
+    background ("black");
+  }
+  
+  function draw() {
+    
+    stroke("aquamarine")
+    fill("indigo")
+    if(mouseIsPressed){
+    rect(mouseX, mouseY, 20, 20,)
+  }
+    
+  }
